18ca3a44ecd9eeb13d7813045e33a059e03797dc
Kevin Savonije <kevin@23g.nl>
2024-11-15 12:03:57 +0100

    chore: clean up more files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
 
 <template>
-    <div>Hello world</div>
+    <header>
+        <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+        </nav>
+    </header>
+
+    <RouterView />
 </template>
