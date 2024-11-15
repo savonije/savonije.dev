795fd2707f4436ff28b737ebe92a3dca1fd1d95e
Kevin Savonije <kevin@23g.nl>
2024-11-15 12:29:20 +0100

    chore config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,37 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from "eslint-plugin-vue";
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
 
 export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
-  },
+    {
+        name: "app/files-to-lint",
+        files: ["**/*.{ts,mts,tsx,vue}"],
+        rules: {
+            "@typescript-eslint/strict-boolean-expressions": "error",
+            "vue/multi-word-component-names": 0,
+            "vue/html-button-has-type": [
+                "error",
+                {
+                    button: true,
+                    submit: true,
+                    reset: true,
+                },
+            ],
+            "vue/block-order": [
+                "error",
+                {
+                    order: ["script", "template", "style"],
+                },
+            ],
+        },
+    },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  },
+    {
+        name: "app/files-to-ignore",
+        ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+    },
 
-  ...pluginVue.configs['flat/essential'],
-  ...vueTsEslintConfig(),
-  skipFormatting,
-]
+    ...pluginVue.configs["flat/essential"],
+    ...vueTsEslintConfig(),
+    skipFormatting,
+];
